f0aa5b35ca428dce7a13ddb000781acd5d06ee3d
Jefferson Savidotti <jeffersonsavidotti@gmail.com>
2024-06-02 01:27:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        // Method
+        showSkills: function() {
+            this.skills.forEach(function(skill) {
+                console.log(skill);
+            });
+        }
+    };
+    return member;
+}
